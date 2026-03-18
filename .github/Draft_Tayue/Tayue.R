641d88a541b220e3b2800478b75deb6f7833156f
Your Full Name <guskebta@student.gu.se>
2026-03-18 17:34:14 +0100

    Final Q2 code

--- .github/Draft_Tayue/Tayue.R
+++ .github/Draft_Tayue/Tayue.R
@@ -3,10 +3,10 @@
 library(data.table)
 library(dplyr)
 
+# Load and Clean
 analysis_data <- fread("data/data_linked.csv")
-head(analysis_data)
-str(analysis_data)
 
+# Creating income quintile
 analysis_data <- analysis_data |>
   mutate(
     income_quintile = factor(
@@ -15,37 +15,41 @@ analysis_data <- analysis_data |>
       labels = c("Lowest", "Low-Middle", "Middle", "High-Middle", "Highest")
     )
   )
+
+# Aggregate to Patient Level
 analysis_data <- analysis_data |>
   group_by(patient, gender, income_quintile) |>
   summarise(
     total_medical_cost = sum(totalcost, na.rm = TRUE),
     base_cost = sum(base_cost, na.rm = TRUE),
-    payer_coverage = sum(payer_coverage, na.rm =TRUE),
+    payer_coverage = sum(payer_coverage, na.rm = TRUE),
     n_encounters = n(),
     .groups = "drop"
   )
 
+# Descriptive Summary Table
 med_cost_summary <- analysis_data |>
-group_by(gender) |>
-summarise(
-avg_med_cost = mean(totalcost, na.rm = TRUE),
-median_med_cost = median(totalcost, na.rm = TRUE),
-n_patients = n()
-)
+  group_by(gender) |>
+  summarise(
+    avg_med_cost = mean(total_medical_cost, na.rm = TRUE),
+    median_med_cost = median(total_medical_cost, na.rm = TRUE),
+    n_patients = n()
+  )
 
 print(med_cost_summary)
 
-# --- Statistical test: compare medication cost by gender ---
-med_cost_ttest <- t.test(medication_cost ~ gender, data = analysis_data)
+# Statistical test: compare medication cost by gender ---
+med_cost_ttest <- t.test(total_medical_cost ~ gender, data = analysis_data)
 print(med_cost_ttest)
 
-# --- Regression: medication cost as outcome ---
+#  Regression: medication cost as outcome ---
 med_cost_model <- glm(
-medication_cost ~ GENDER + AGE + PAYER,
-family = Gamma(link = "log"),
-data = analysis_data
+  total_medical_cost ~ gender + income_quintile, 
+  family = Gamma(link = "log"), 
+  data = analysis_data |> filter(total_medical_cost > 0)
 )
 
 summary(med_cost_model)
 
-unique(analysis_data$gender)
+
+# Q4 
